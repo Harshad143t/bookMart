100a49350a4ba289e3120bfb94b532f0681e8ea3
Harshad Ingle <imironman0078@gmail.com>
2026-09-09 02:42:42 +0530

    Update Google Apps Script Web App URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste your deployed Google Apps Script Web App URL below.
-window.BOOKMART_SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbw5SXYpQzX39UYIg28BuDq_gl1AaSfhJSiLI4-ib4zJWV3cP1ghVvmjOeadwlEpemRZ/exec";
+window.BOOKMART_SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbwjPLKlLtGU5gL5Qw46CudV0jAmEzWGIxvHkETRJsJDFKp-aQZDG0YzNAmjimS7BNKL/exec";
